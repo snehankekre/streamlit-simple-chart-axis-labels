b629dfc94c53c3a186aeb36451ebc3794af39ae8
snehankekre <snehan.minerva@gmail.com>
2024-05-09 14:20:24 +0800

    Update whl

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,7 +3,7 @@ import pandas as pd
 import streamlit as st
 
 st.set_page_config(
-    page_title="Simple Chart Axis Labels",
+    page_title="Simple Chart Axis Titles",
     page_icon=":bar_chart:",
 )
 
@@ -24,17 +24,17 @@ def load_data():
     return pd.DataFrame(np.random.randn(20, 3), columns=["a", "b", "c"])
 
 
-st.subheader("Customizing Axis Labels for Streamlit's Simple Charts")
+st.subheader("Customizing Axis Titles for Streamlit's Simple Charts")
 
 with st.expander("Context :books:"):
     st.write(
         """
-    Streamlit's [simple charts](https://docs.streamlit.io/develop/api-reference/charts#simple-chart-elements) (line, area, bar, scatter) have a `title_x` and `title_y` parameters that allow you to customize the axis labels.
+    Streamlit's [simple charts](https://docs.streamlit.io/develop/api-reference/charts#simple-chart-elements) (line, area, bar, scatter) have `x_title` and `y_title` parameters that allow you to customize the axis titles.
 
     | Parameter | Description |
     | --- | --- |
-    | `title_x` *(str, or None)* | The title of the x-axis. If None, either the column name specified in ``x`` will be used, or no title will be displayed. |
-    | `title_y` *(str, or None)* | The title of the y-axis. If None, either the column name specified in ``y`` will be used, or no title will be displayed. |
+    | `x_title` *(str, or None)* | The title of the x-axis. If None, either the column name specified in ``x`` will be used, or no title will be displayed. |
+    | `y_title` *(str, or None)* | The title of the y-axis. If None, either the column name specified in ``y`` will be used, or no title will be displayed. |
 
 
     """
@@ -49,7 +49,7 @@ with st.expander("Context :books:"):
 
     st.write(
         """
-    - Notion project: [Built-in charts: Customizable axis labels](https://www.notion.so/snowflake-corp/Product-spec-202a693205e046e28c451bc43d18f11c)
+    - Notion project: [Built-in charts: Customizable axis titles](https://www.notion.so/snowflake-corp/Product-spec-202a693205e046e28c451bc43d18f11c)
     - Download the [wheel file](https://github.com/snehankekre/streamlit-simple-chart-axis-labels/raw/main/streamlit-1.33.0-py2.py3-none-any.whl)
     - See the PoC implementation: https://github.com/streamlit/streamlit/compare/develop...snehan/feature/chart-titles
     """
@@ -63,14 +63,14 @@ with st.expander("Context :books:"):
 #     y="col2",
 #     color="col3",
 #     use_container_width=True,
-#     title_x="This is the x-axis title",
+#     x_title="This is the x-axis title",
 # )
 
 
 chart_data = load_data()
 
-title_x = st.text_input(label="Enter the x-axis title", value="This is the x-axis title", help="This is the x-axis title that will be displayed below the x-axis.")
-title_y = st.text_input(label="Enter the y-axis title", value="This is the y-axis title", help="This is the y-axis title that will be displayed to the left of the y-axis.")
+x_title = st.text_input(label="Enter the x-axis title", value="This is the x-axis title", help="This is the x-axis title that will be displayed below the x-axis.")
+y_title = st.text_input(label="Enter the y-axis title", value="This is the y-axis title", help="This is the y-axis title that will be displayed to the left of the y-axis.")
 
 tab1, tab2, tab3, tab4 = st.tabs(["Line Chart", "Area Chart", "Bar Chart", "Scatter Chart"])
 
@@ -80,18 +80,18 @@ with tab1:
         x="a",
         y="b",
         use_container_width=True,
-        title_x=title_x,
-        title_y=title_y,
+        x_title=x_title,
+        y_title=y_title,
     )
     st.code(
-    """
+    f"""
     st.line_chart(
         chart_data,
         x="a",
         y="b",
         use_container_width=True,
-        title_x="This is the x-axis title",
-        title_y="This is the y-axis title",
+        x_title="{x_title}",
+        y_title="{y_title}",
     )
     """
     )
@@ -100,16 +100,16 @@ with tab2:
     st.area_chart(
         chart_data,
         use_container_width=True,
-        title_x=title_x,
-        title_y=title_y,
+        x_title=x_title,
+        y_title=y_title,
     )
     st.code(
-    """
+    f"""
     st.area_chart(
         chart_data,
         use_container_width=True,
-        title_x="This is the x-axis title",
-        title_y="This is the y-axis title",
+        x_title="{x_title}",
+        y_title="{y_title}",
     )
     """
     )
@@ -118,17 +118,17 @@ with tab3:
     st.bar_chart(
         chart_data,
         use_container_width=True,
-        title_x=title_x,
-        title_y=title_y,
+        x_title=x_title,
+        y_title=y_title,
     )
 
     st.code(
-    """
+    f"""
     st.bar_chart(
         chart_data,
         use_container_width=True,
-        title_x="This is the x-axis title",
-        title_y="This is the y-axis title",
+        x_title="{x_title}",
+        y_title="{y_title}",
     )
     """
     )
@@ -136,20 +136,19 @@ with tab4:
     st.scatter_chart(
         chart_data,
         use_container_width=True,
-        title_x=title_x,
-        title_y=title_y,
+        x_title=x_title,
+        y_title=y_title,
     )
     st.code(
-    """
+    f"""
     st.scatter_chart(
         chart_data,
         use_container_width=True,
-        title_x="This is the x-axis title",
-        title_y="This is the y-axis title",
+        x_title="{x_title}",
+        y_title="{y_title}",
     )
     """
     )
-
 
 
 
